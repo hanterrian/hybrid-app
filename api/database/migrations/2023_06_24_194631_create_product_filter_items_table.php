25c62409451ae01ee1e9810ba841825720253097
hanterrian <hanterrian@gmail.com>
2023-06-24 19:47:42 +0300

    Add product filter option

---
+++ api/database/migrations/2023_06_24_194631_create_product_filter_items_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('product_filter_items', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignIdFor(\App\Models\Category::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\FilterOption::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\CategoryFilterItem::class)->constrained()->cascadeOnDelete();
+
+            $table->string('value')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('product_filter_items');
+    }
+};
